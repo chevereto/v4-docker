bf3ca925b7f3f38ac0ff7361f4f5bc1df3f6cf26
Rodolfo Berrios <inbox@rodolfoberrios.com>
2021-04-09 08:51:07 -0400

    support running from any path

--- bin/imaginery.sh
+++ bin/imaginery.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+PROJECT="$(dirname $DIR)"
 echo '* Building v3-httpd-php'
-docker build -t chevereto:v3-httpd-php httpd-php
+docker build -t chevereto:v3-httpd-php "$PROJECT/"httpd-php
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+    exit $RESULT
+fi
 echo '* Building v3-demo'
-docker build -t chevereto:v3-demo demo
+docker build -t chevereto:v3-demo "$PROJECT/"demo
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+    exit $RESULT
+fi
 echo '* Building v3-httpd'
-docker build -t chevereto:v3-httpd httpd
+docker build -t chevereto:v3-httpd "$PROJECT/"httpd
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+    exit $RESULT
+fi
 echo '* Building v3-php-fpm'
-docker build -t chevereto:v3-php-fpm php-fpm
-printf '\n💯 Chevereto imagenery done!\n'
+docker build -t chevereto:v3-php-fpm "$PROJECT/"php-fpm
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+    exit $RESULT
+fi
+printf '[OK] Chevereto imagenery done!\n'
