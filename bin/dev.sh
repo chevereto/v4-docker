82a1579f99055a228785e7bb373e23d22b59f9ac
Rodolfo Berrios <inbox@rodolfoberrios.com>
2021-06-13 18:08:13 -0400

    wip

--- bin/dev.sh
+++ bin/dev.sh
@@ -56,13 +56,6 @@ if [ $RESULT -eq 1 ]; then
     GRANT ALL ON chevereto.* TO 'chevereto' IDENTIFIED BY 'user_database_password';"
 fi
 echo "* Provide v3-httpd-php"
-# -e "CHEVERETO_ASSET_STORAGE_NAME=aws-us-west-1" \
-# -e "CHEVERETO_ASSET_STORAGE_TYPE=s3" \
-# -e "CHEVERETO_ASSET_STORAGE_KEY=key" \
-# -e "CHEVERETO_ASSET_STORAGE_SECRET=secret" \
-# -e "CHEVERETO_ASSET_STORAGE_BUCKET=bucket" \
-# -e "CHEVERETO_ASSET_STORAGE_URL=https://s3-us-west-1.amazonaws.com/bucket/" \
-# -e "CHEVERETO_ASSET_STORAGE_REGION=us-west-1" \
 docker run -d \
     -p "$PORT:80" \
     -e "CHEVERETO_DB_HOST=dev-mariadb" \
@@ -72,10 +65,13 @@ docker run -d \
     -e "CHEVERETO_TAG=dev" \
     -e "CHEVERETO_ASSET_STORAGE_NAME=dev-assets" \
     -e "CHEVERETO_ASSET_STORAGE_TYPE=local" \
+    -e "CHEVERETO_ASSET_STORAGE_URL=http://localhost:$PORT/public_assets" \
+    -e "CHEVERETO_ASSET_STORAGE_BUCKET=/var/www/html/public_assets/" \
     -e "CHEVERETO_IMAGE_LIBRARY=gd" \
     --name chv-dev \
     --network chv-network \
     --network-alias dev \
+    --mount src="/var/www/html/chevereto.loc/public_assets",target=/var/www/html/public_assets,type=bind \
     --mount src="/var/www/html/chevereto.loc/public_html",target=/var/www/html,type=bind \
     --mount src="/var/www/html/chevereto.loc/public_html/images",target=/var/www/html/images,type=bind \
     --mount src="/var/www/html/chevereto.loc/public_html/importing/no-parse",target=/var/www/html/importing/no-parse,type=bind \
@@ -83,7 +79,7 @@ docker run -d \
     --mount src="/var/www/html/chevereto.loc/public_html/importing/parse-albums",target=/var/www/html/importing/parse-albums,type=bind \
     chevereto/chevereto:latest-httpd-php >/dev/null 2>&1
 echo '* Applying permissions'
-docker exec -it chv-dev bash -c "chown www-data: . -R"
+# docker exec -it chv-dev bash -c "chown www-data: . -R"
 if [ "$createDev" != "${createDev#[Yy]}" ]; then
     echo "* Creating dev:password"
     docker exec -d chv-dev \
