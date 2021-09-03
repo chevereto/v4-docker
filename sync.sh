106f441858d3ff4f334a82b8a88c406ae9a6caa5
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2021-09-03 13:24:17 -0400

    ⚡️me saqué un regio syncing!

--- sync.sh
+++ sync.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 set -e
+cp /var/www/chevereto/.gitignore /var/www/html/.gitignore
 function sync() {
     rsync -r -I -og \
         --chown=www-data:www-data \
         --info=progress2 \
         --filter=':- .gitignore' \
         --exclude '.git' \
-        --exclude 'importing/' \
-        --exclude 'images/' \
-        --exclude '_assets/' \
         --delete \
         /var/www/chevereto/ /var/www/html/
 }
 sync
-while inotifywait -r -e modify,create,delete /var/www/chevereto/; do
+EXCLUDE=$(
+    readarray -t ARRAY </var/www/chevereto/.gitignore
+    IFS='|'
+    echo "${ARRAY[*]}"
+)"|\.git"
+while inotifywait --exclude ${EXCLUDE} -r -e modify,create,delete /var/www/chevereto/; do
     sync
 done
