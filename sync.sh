932ff8e0c3de4eee5826b984e173cfc66c59cebd
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2021-09-03 11:59:49 -0400

    👀 sync 1s, excludes

--- sync.sh
+++ sync.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 set -e
-while inotifywait -r -e modify,create,delete /var/www/chevereto/; do
+function sync() {
     rsync -r -I -og \
         --chown=www-data:www-data \
         --info=progress2 \
         --filter=':- .gitignore' \
         --exclude '.git' \
+        --exclude 'importing/' \
+        --exclude 'images/' \
+        --exclude '_assets/' \
         --delete \
         /var/www/chevereto/ /var/www/html/
+}
+sync
+while inotifywait -r -e modify,create,delete /var/www/chevereto/; do
+    sync
 done
