2aaed140cd1d1bf98adf14f4007b860fa3bfb562
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2021-10-25 09:06:11 -0300

    improves sync

--- sync.sh
+++ sync.sh
@@ -2,22 +2,29 @@
 set -e
 SOURCE=/var/www/chevereto/
 TARGET=/var/www/html/
+EXCLUDE="\.git|\.DS_Store|\.vscode|\/app\/vendor|\/app\/settings\.php|\/app\/importer\/jobs"
 cp "${SOURCE}".gitignore "${TARGET}".gitignore
-EXCLUDE=$(
-    readarray -t ARRAY <"${SOURCE}".gitignore
-    IFS='|'
-    echo "${ARRAY[*]}"
-)"|\.git"
 function sync() {
     rsync -r -I -og \
         --chown=www-data:www-data \
         --info=progress2 \
         --filter=':- .gitignore' \
         --exclude '.git' \
+        --exclude '_assets/' \
         --delete \
         $SOURCE $TARGET
 }
 sync
-while inotifywait --exclude ${EXCLUDE} -r -e modify,create,delete ${SOURCE}; do
-    sync
-done
+inotifywait \
+    --event create \
+    --event delete \
+    --event modify \
+    --event move \
+    --format "%e %w%f" \
+    --exclude $EXCLUDE \
+    --monitor \
+    --recursive \
+    $SOURCE |
+    while read CHANGED; do
+        sync
+    done
